d78db9980bfeca41bb5262d119f2bd06d7978d61
HoltWeiyangWu <holtwyw@hotmail.com>
2024-07-02 17:52:33 +1000

    Initialise project backend

---
+++ src/main/java/holt/Main.java
@@ -0,0 +1,12 @@
+package holt;
+
+/**
+ * @author Weiyang Wu
+ * @date 2024/7/2 17:24
+ * Student ID: 1171826
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
